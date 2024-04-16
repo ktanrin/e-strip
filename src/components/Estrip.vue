abe39884abb48644f26ced19202b3e1aef4812a4
ktanrin <ktanrin@gmail.com>
2024-04-16 19:52:28 +0700

    Update Estrip.vue

---
+++ src/components/Estrip.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="estrip">
+        <!-- Your code here -->
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Estrip',
+    data() {
+        return {
+            state: '',
+            requestedFlightLevel: '',
+            aircraftCallsign: '',
+            Type: '',
+            WTC: '',
+            SSR: '',
+            TOBT: '',
+            destinationAirportICAOCode: '',
+            stand: '',
+            flightRule: '',
+            runwayInUse: '',
+            SID: '',
+            taxiRoute: ''
+        };
+    }
+};
+</script>
+
+<style scoped>
+.estrip {
+    /* Your styles here */
+}
+</style>
